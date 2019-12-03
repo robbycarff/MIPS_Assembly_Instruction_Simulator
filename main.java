fd8d62547e4905f01b22d5374603ad2aa3cf8c95
Robert <rpcarff@gmail.com>
2019-12-03 12:20:49 -0500

    adding 5 stages

--- main.java
+++ main.java
@@ -1,42 +1,109 @@
 
-/****************************************************************************
+/******************************************************************************
 Written by: Robert Carff
 Date: November 26, 2019
-Use: written for Dr. Buells 513 Computer Archtecture class 
-*****************************************************************************/
+Use: written for Dr. Buells 513 Computer Archtecture class, project 4
+
+// each method could be a stage 
+// 5 steps 5 methods
+
+*******************************************************************************/
 /* IMPORTS */
 import java.io.*;
 import java.util.*;
-
 /* MAIN CLASS */
 public class Main {
-
 	/****************************************************************************
-	This is where we read in the text file 
-	we're assuming our instructions are 32 bit binary MIPS
+	* gobal data structures and resources declared here
 	****************************************************************************/
-	public static void readBinary() {
-		//read in 1's 0's here
+	/* RESOURCE VARIABLES */
+	int _if_  = 0;
+	int _id_  = 1;
+	int _ex_  = 2;
+	int _mem_ = 3;
+	int _wb_  = 4;
+	/* REGISTERS */
+	int r1 = 0;
+	int r2 = 0;
+	int r3 = 0;
+	/* LISTS */
+	private static ArrayList<ArrayList<String>> instructionList = new ArrayList<ArrayList<String>>();
+	/* DICTIONARY */
+
+
+	/* PRINT FUNCTION */
+	public static void printinstructionList(){
+		System.out.println("** DECODED INSTRUCTIONS **");
+		for(ArrayList line : instructionList)
+		  { 		      
+	           System.out.println(line); 		
+	      }
+	      System.out.println("\n");
+	}
+	/****************************************************************************
+	* we're assuming our instructions are 32 bit binary MIPS
+	* IF STAGE - fetching 32 bit lines from a piped in file 
+	* feeds the 32 bit string to the decode method
+	****************************************************************************/
+	public static void readBinaryIF() {
 		Scanner scan = new Scanner(System.in);
-		System.out.println("\nReading in file\n");
+		//for each line (each instruction)
 		while(scan.hasNextLine()){
-			String checkLine = scan.nextLine();
+			//this is a binary 32-bit instruction (FETCH)
 			try {
-				// do something with 1's 0's here
-				System.out.println(checkLine);
-				System.out.println("\n");
+				/* IF STAGE */
+				String binaryString = scan.nextLine();
+				System.out.println(binaryString); 
+				/* DECODE STAGE) */
+				BinarytoMipsDC(binaryString);
+				/* EXECUTE STAGE */
+
+				//
+				
 			} catch(Exception e) {
 				System.out.println("its broken");
 
 			}
 		}
+	}
+	/****************************************************************************
+	* BinarytoMips() takes a 32 bit string (binary) and returns the instruction
+	* DECODE STAGE
+	****************************************************************************/
+	public static void BinarytoMipsDC(String binaryString) {
+		// declaring our list that will hold our 32 bit decoded instruction
+		ArrayList<String> decodedMips = new ArrayList<>();
+		
+
+		//begin converting here
+		System.out.println(binaryString);
+		
+
+		//adding our decoded instruction to the list
+		instructionList.add(decodedMips);
+
+	}
+
+	/****************************************************************************
+	* DoTheWork() takes the converted list of lists and schedules
+	* EXECUTE STAGE
+	* MEMORY STAGE ??
+	****************************************************************************/
+	public static void DoTheWork() {
 
 	}
 	/****************************************************************************
-	The main method is where we can choose functions called to do the work
+	* prints out our final formatted answer
+	****************************************************************************/
+
+	/****************************************************************************
+	* The main() runs our methods + formats
 	****************************************************************************/
 	public static void main(String[] args){
-		readBinary(); 
+		readBinaryIF();
+		DoTheWork(); 
+		printinstructionList();
+
 
 	}
 }
